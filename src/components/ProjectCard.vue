f8e1c7696b66911a7e0ca9307dc09f5f73a1e116
Luigi <luigipaglionicowd@gmail.com>
2022-02-11 16:17:25 +0100

    added dynamic cards in projects

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :id="cardId" class="card">
+        <div class="front flex flex-jc-c flex-ai-c">
+        <div class="front-bg"></div>
+        <div class="card-text">
+            <p v-for="title in cardFrontTitle" :key="title">
+                {{ title }}
+            </p>
+        </div>
+        </div>
+        <div class="back">
+        <h3>{{ cardName }}</h3>
+        <p>{{ cardDesc }}</p>
+        <div class="desc-container flex flex-jc-c flex-ai-c">
+            <div class="desc-text">
+            <p>Competenze:</p>
+            <div class="desc-list flex flex-jc-c">
+                <ul>
+                <li v-for="skill in cardSkillsList" :key="skill">
+                    {{ skill }}
+                </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+        <div class="github-link flex" :class="cardLinks.length > 1 ? 'flex-jc-sa' : 'flex-jc-c'">
+            <a v-for="link in cardLinks" :key="link"
+            :href="link" target="_blank">
+                <i class="fab fa-github" aria-hidden="true" v-if="link.includes('github.com')"></i>
+                <i class="fas fa-external-link-alt" v-if="link.includes('github.io')"></i>
+            </a>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        cardList: Object,
+    },
+    data() {
+        return {
+            cardId: this.cardList.cardId,
+            cardFrontTitle: this.cardList.cardFrontTitle,
+            cardName: this.cardList.cardName,
+            cardDesc: this.cardList.cardDesc,
+            cardSkillsList: this.cardList.cardSkillsList,
+            cardLinks: this.cardList.cardLinks,
+        }
+    },
+}
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,36 +5,9 @@
           <hr class="titlehr">
 
           <div class="card-row flex flex-jc-sb flex-ai-c">
-            <div id="portfolio-card" class="card">
-              <div class="front flex flex-jc-c flex-ai-c">
-                <div class="front-bg"></div>
-                <div class="card-text">
-                  <p>Portfolio</p>
-                </div>
-              </div>
-              <div class="back">
-                <h3>Portfolio</h3>
-                <p>Il <span>portfolio</span> è stato il mio primo sito idealizzato e sviluppato, è stato creato a Febbraio 2021 all'età di 17 anni.</p>
-                <div class="desc-container flex flex-jc-c flex-ai-c">
-                  <div class="desc-text">
-                    <p>Competenze:</p>
-                    <div class="desc-list flex flex-jc-c">
-                      <ul>
-                        <li>HTML</li>
-                        <li>SASS</li>
-                        <li>PHP</li>
-                        <li>VueJS</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div class="github-link flex flex-jc-c">
-                  <a href="https://github.com/LuigiP2404/Portfolio" target="_blank"><i class="fab fa-github" aria-hidden="true"></i></a>
-                </div>
-              </div>
-            </div>
-
-            <div id="monster-card" class="card">
+            <project-card :cardList="cardList[0]" />
+            <project-card :cardList="cardList[1]" />
+            <!-- <div id="monster-card" class="card">
               <div class="front flex flex-jc-c flex-ai-c">
                 <div class="front-bg"></div>
                 <div class="card-text">
@@ -64,11 +37,13 @@
                   <a href="https://luigip2404.github.io/Monster-vs-Player/" target="_blank"><i class="fas fa-external-link-alt"></i></a>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
 
           <div class="card-row flex flex-jc-sb flex-ai-c">
-            <div id="todo-card" class="card">
+            <project-card :cardList="cardList[2]" />
+            <project-card :cardList="cardList[3]" />
+            <!-- <div id="todo-card" class="card">
               <div class="front flex flex-jc-c flex-ai-c">
                 <div class="front-bg"></div>
                 <div class="card-text">
@@ -95,9 +70,9 @@
                   <a href="https://luigip2404.github.io/todo-list" target="_blank"><i class="fas fa-external-link-alt"></i></a>
                 </div>
               </div>
-            </div>
+            </div> -->
 
-            <div id="calcolatrice-card" class="card">
+            <!-- <div id="calcolatrice-card" class="card">
               <div class="front flex flex-jc-c flex-ai-c">
                 <div class="front-bg"></div>
                 <div class="card-text">
@@ -124,18 +99,95 @@
                   <a href="https://luigip2404.github.io/Calcolatrice/" target="_blank"><i class="fas fa-external-link-alt"></i></a>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
         </div>
       </section>
 </template>
 
 <script>
+import ProjectCard from './ProjectCard.vue';
+
 export default {
     name: 'Projects',
+    components: {
+      ProjectCard,
+    },
     data() {
         return {
-
+          cardList: [
+            {
+              cardFrontTitle: [
+                'Portfolio',
+              ],
+              cardName: 'Portfolio',
+              cardId: 'portfolio-card',
+              cardDesc: "Il <span>portfolio</span> è stato il mio primo sito idealizzato e sviluppato, è stato creato a Febbraio 2021 all'età di 17 anni.",
+              cardSkillsList: [
+                'HTML',
+                'SASS',
+                'VueJS',
+                'PHP'
+              ],
+              cardLinks: [
+                'https://github.com/LuigiP2404/Portfolio',
+              ],
+            },
+            {
+              cardFrontTitle: [
+                'Monster',
+                ' vs ',
+                'Player'
+              ],
+              cardName: 'Monster vs Player',
+              cardId: 'monster-card',
+              cardDesc: `<span>Monster vs Player</span> è una web app, per esattezza un piccolo gioco sviluppato in Vue JS, in cui ci sono un Giocatore
+                  ed un Mostro, con dei bottoni per effettuare diverse azioni.`,
+              cardSkillsList: [
+                'HTML',
+                'CSS',
+                'VueJS',
+              ],
+              cardLinks: [
+                'https://github.com/LuigiP2404/Player-vs-Monster',
+                'https://luigip2404.github.io/Monster-vs-Player/'
+              ],
+            },
+            {
+              cardFrontTitle: [
+                'Portfolio',
+              ],
+              cardName: 'Portfolio',
+              cardId: 'portfolio-card',
+              cardDesc: "Il <span>portfolio</span> è stato il mio primo sito idealizzato e sviluppato, è stato creato a Febbraio 2021 all'età di 17 anni.",
+              cardSkillsList: [
+                'HTML',
+                'SASS',
+                'VueJS',
+                'PHP'
+              ],
+              cardLinks: [
+                'https://github.com/LuigiP2404/Portfolio',
+              ],
+            },
+            {
+              cardFrontTitle: [
+                'Portfolio',
+              ],
+              cardName: 'Portfolio',
+              cardId: 'portfolio-card',
+              cardDesc: "Il <span>portfolio</span> è stato il mio primo sito idealizzato e sviluppato, è stato creato a Febbraio 2021 all'età di 17 anni.",
+              cardSkillsList: [
+                'HTML',
+                'SASS',
+                'VueJS',
+                'PHP'
+              ],
+              cardLinks: [
+                'https://github.com/LuigiP2404/Portfolio',
+              ],
+            },
+          ]
         }
     }
 }
